64018ae439dfd3761243ce7a32a24e1d1933663c
ychartois <yannickchartois@wanadoo.fr>
2012-09-01 17:17:19 +0200

    Réorganisation des packages views

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,28 +3,50 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jboss.netty.handler.ssl.SslBufferPool;
-
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
+import views.html.welcome.welcome;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-	  
-    return ok(index.render("Your new application is ready.", getWorkshops()));
-  }
- 
-  public static List<String> getWorkshops(){
-	  List<String> wks = new ArrayList<String>();
-	  for (int i=1; i<=5; i++) {
-		  StringBuffer sb = new StringBuffer();
-		  sb.append("WorksShop_");
-		  sb.append(i);
-		  wks.add(sb.toString());
-	  }
-	  return wks;
-  }
-  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+	
+	// <--------------------------------------------------------------------------->
+	// - 							Actions Methods
+	// <--------------------------------------------------------------------------->	
+	/**
+	 * This method is the action that render the welcome page
+	 * 
+	 * @return Result the http response
+	 */
+	public static Result welcome() {	
+		// We render the welcome page
+		return ok(welcome.render("Your new application is ready.", getWorkshops()));
+	}
+
+	
+	// <--------------------------------------------------------------------------->
+	// - 							helper methods
+	// <--------------------------------------------------------------------------->	
+	public static List<String> getWorkshops() {
+		List<String> wks = new ArrayList<String>();
+		for (int i = 1; i <= 5; i++) {
+			StringBuffer sb = new StringBuffer();
+			sb.append("WorksShop_");
+			sb.append(i);
+			wks.add(sb.toString());
+		}
+		return wks;
+	}
+
+	
+	// <--------------------------------------------------------------------------->
+	// - 							Constructor(s)
+	// <--------------------------------------------------------------------------->
+	/**
+	 * Constructor by default
+	 */
+	public Application() {
+		super();
+	}
+
 }
