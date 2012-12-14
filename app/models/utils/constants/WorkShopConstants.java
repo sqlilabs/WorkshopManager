7c46b6379b31cdf4bbb3fc7a919482806cdb5d81
ychartois <yannickchartois@wanadoo.fr>
2012-12-14 16:26:12 +0100

    the missing constants...

---
+++ app/models/utils/constants/WorkShopConstants.java
@@ -0,0 +1,32 @@
+package models.utils.constants;
+
+public class WorkShopConstants {
+
+	
+	//<--------------------------------------------------------------------------->
+	//-							 	Constants	        
+	//<--------------------------------------------------------------------------->
+	/**
+	 * Fake ID pour dire que l'objet n'existe pas.
+	 */
+	public static final long		ID_NOT_IN_TABLE		=	-1;
+	
+	/**
+	 * DATE_PATTERN pattern to convert date to String
+	 */
+	public static final String 	DATE_PATTERN 		= 	"dd/MM/yyyy";
+	
+	
+	
+	
+	//<--------------------------------------------------------------------------->
+	//-							 Constructeur(s)	        
+	//<--------------------------------------------------------------------------->	
+	/**
+	 * Constructor private because it's a constant class
+	 */
+	private WorkShopConstants() {
+		super();
+	}
+
+}
