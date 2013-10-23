1143e5096f266d1aa1d542bc33fb571984e526cf
ychartois <yannickchartois@wanadoo.fr>
2013-10-23 11:19:39 +0200

    Issue #66 Prepare a real sql script with fake data to help the tests Add a fake user which represents an external user

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,3 @@
+# --- !Ups
+INSERT INTO `USER` (`id`, `first_name`, `last_name`, `email`, `picture`, `role`, `charterAgree`) VALUES
+(1, 'External', 'User', 'fakeUser@test.com', 'http://i.stack.imgur.com/TCK6x.jpg?s=128&g=1', 'standard', 1);
